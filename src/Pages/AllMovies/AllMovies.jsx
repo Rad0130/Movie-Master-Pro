d6a4a98d1934b6dca8c402bfd8f8c9f3978cab84
Your Name <your.email@example.com>
2025-11-13 05:32:30 +0600

    initial setup added

--- src/Pages/AllMovies/AllMovies.jsx
+++ src/Pages/AllMovies/AllMovies.jsx
@@ -6,6 +6,13 @@ const AllMovies = () => {
     const [allMovies,setAllMovies]=useState([]);
     const [loading,setLoading]=useState(true);
 
+    const genres=[]
+    for(const movie of allMovies){
+        if(!genres.includes(movie.genre)){
+            genres.push(movie.genre);
+        }
+    }
+
     useEffect(()=>{
         setLoading(true);
         fetch('http://localhost:3000/movies')
@@ -26,6 +33,11 @@ const AllMovies = () => {
     return (
         <div className='mt-30'>
             <h1 className='font-bold text-4xl'>All Movies</h1>
+            <div className='flex gap-2 mt-5'>
+                {
+                    genres.map(genre=><button className="btn btn-outline btn-secondary">{genre}</button>)
+                }
+            </div>
             <div className='grid geid-cols-1 md:grid-cols-5 gap-5 my-10'>
                 {
                     allMovies.map(movie=><SingleMovie movie={movie}></SingleMovie>)
